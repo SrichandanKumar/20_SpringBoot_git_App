bc88299dc790897baabb859127acffcb022f5f9f
SrichandanKumar <bapunkumar835@email.com>
2021-06-26 16:57:13 +0530

    HIS-134 fix provided

--- src/main/java/in/Insurance/Application.java
+++ src/main/java/in/Insurance/Application.java
@@ -10,6 +10,8 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 	
+		//changes for HIS-134
+		
 	context.close();
 	
 	
